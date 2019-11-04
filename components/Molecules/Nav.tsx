448b9d3672af39d06a1db1e5097bf7c6188b8c56
macotok <monochromer@r8.ucom.ne.jp>
2019-11-04 20:23:28 +0900

    navにbootstrapを適用

--- components/Molecules/Nav.tsx
+++ components/Molecules/Nav.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
 import Link from 'next/link';
 
+interface NavList {
+  href: string;
+  text: string;
+}
+
+const navList = [
+  {
+    href: '/',
+    text: 'Home'
+  },
+  {
+    href: '/about',
+    text: 'About'
+  },
+  {
+    href: '/login',
+    text: 'Login'
+  },
+]
+
+
 const Nav: React.FunctionComponent = (): JSX.Element => (
   <nav>
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/login">
-          <a>Login</a>
-        </Link>
-      </li>
+    <ul className="nav nav-pills nav-fill">
+      {
+        navList.map((nav: NavList, index: number) => (
+          <li className="nav-item" key={index}>
+            <Link href={nav.href}>
+              <a className={`nav-link ${index === 0 ? 'active' : ''}`}>{nav.text}</a>
+            </Link>
+          </li>
+        ))
+      }
     </ul>
   </nav>
 );
